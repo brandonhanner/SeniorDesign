7542bf66e150fd82479675069901814345676071
Casey Hanner <brandon.hanner@mavs.uta.edu>
2018-12-03 04:42:11 +0000

    Messing with timertest

--- Aeroneers/classes/acquisition/timertest.cpp
+++ Aeroneers/classes/acquisition/timertest.cpp
@@ -24,11 +24,12 @@ int main()
   float min = 100.0;
   float maxdelt = 0.0;
   float mindelt = 100.0;
-  int numberOfIterations = 1000;
+  int numberOfIterations = 1500;
+  uint32_t msec_curr = 0;
 
   while (i<numberOfIterations)
   {
-    uint32_t msec_curr = millis();
+    msec_curr = millis();
 
     if (msec_curr-msec_prev > 8) {
 	start = millis();
@@ -45,12 +46,12 @@ int main()
         if (end - start < min) min = end-start;
         if (delta_t > maxdelt) maxdelt = (float) delta_t;
 	if (delta_t < mindelt) mindelt = (float) delta_t;
-        printf(" %f -- %d -- %d\n",curVal, end-start ,delta_t);
+        printf(" Accel: %+-8f \t loop: %-8d \t delta: %-8d\n",curVal, end-start ,delta_t);
         last_round = millis();
         //yn = w × xn + (1 – w) × yn – 1
     };
   }
   //time0 holds delta_ts so avg time to get back, prevTime holds loop times so print statement prints avg time to get back to the loop, avg time spent in the loop, and average total time.
-  printf("%f , %f, %f\n", time0/(float)numberOfIterations, ((float)prevTime)/(float)numberOfIterations, (time0/(float)numberOfIterations)+(((float)prevTime)/(float)numberOfIterations));
+  printf("avg delta: %f , avg loop: %f, avg total: %f\n", time0/(float)numberOfIterations, ((float)prevTime)/(float)numberOfIterations, (time0/(float)numberOfIterations)+(((float)prevTime)/(float)numberOfIterations));
   printf("max loop: %f, min loop: %f, max delt: %f, min delt: %f\n", max, min, maxdelt, mindelt);
 }
