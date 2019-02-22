05c19f5ed7e058d0e169d9c0eaa3b271fde15fb3
Brandon Hanner <brandon.hanner@mavs.uta.edu>
2019-02-22 12:03:17 -0600

    fixed blink to actually work

--- Aeroneers/teensy3.2blink/teensy3.2blink.ino
+++ Aeroneers/teensy3.2blink/teensy3.2blink.ino
@@ -1,19 +1,12 @@
 #define LED_PIN 13
 void setup() {
   // put your setup code here, to run once:
-  pinMode(6,INPUT);
-  pinMode(7,INPUT);
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (digitalRead(6)==LOW)
-  {
-    digitalWrite(LED_PIN,HIGH);
-  }
-  if (digitalRead(7)==LOW)
-  {
-    digitalWrite(LED_PIN,LOW);
-  }
+  digitalWrite(LED_PIN,HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN,LOW);
 }
